8b25cd5ada84966b308a3c36e4b1bb0556d75359
Nathan <76179797+Nanuuxz@users.noreply.github.com>
2022-12-24 17:29:35 -0300

    Código em Python

---
+++ código.py
@@ -0,0 +1,20 @@
+metros = float(input('Digite uma distância em metros: '))
+
+km = metros/1000
+hm = metros/100
+dam = metros/10
+m = metros*1
+dm = metros*10
+cm = metros*100
+mm = metros*1000
+
+
+print(km,'km')
+print(hm,'hm')
+print(dam,'dam')
+print(m,'m')
+print(dm,'dm')
+print(cm,'cm')
+print(mm,'mm')
+print(' ')
+print('By Nanuuxz')
